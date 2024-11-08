6207fa71cd801272dfd372175e55030ddbc3699a
Hiramrr <sasuke_77.23@hotmail.com>
2024-11-08 12:10:24 -0600

    Nuevo

---
+++ src/main/java/com/mycompany/pokemonbd/PokemonBD.java
@@ -0,0 +1,13 @@
+package com.mycompany.pokemonbd;
+
+/**
+ *
+ * @author sasuk
+ */
+public class PokemonBD {
+
+    public static void main(String[] args) {
+       PInicio p = new PInicio();
+       p.setVisible(true);
+    }
+}
