f67a0272a7b1504e0faa705372dfe60c2571f2b5
Hiramrr <sasuke_77.23@hotmail.com>
2024-11-26 12:42:14 -0600

    resolucion otra vez

--- src/main/java/com/mycompany/pokemonbd/crear_entrenador.java
+++ src/main/java/com/mycompany/pokemonbd/crear_entrenador.java
@@ -82,7 +82,8 @@ public class crear_entrenador extends javax.swing.JPanel implements ActionListen
         entrenadorID.setForeground(new java.awt.Color(255, 255, 255));
         entrenadorID.setBorder(null);
         entrenadorID.setCaretColor(new java.awt.Color(13, 17, 23));
-        entrenadorID.setDisabledTextColor(new java.awt.Color(255, 255, 255));
+        entrenadorID.setDisabledTextColor(new java.awt.Color(13, 17, 23));
+        entrenadorID.setEnabled(false);
         entrenadorID.setSelectedTextColor(new java.awt.Color(13, 17, 23));
         entrenadorID.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
